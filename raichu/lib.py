76dfb711118c542227959ed0c1778355f6e46f21
Alexandre Monnier <ironex@gmail.com>
2021-11-15 18:41:49 +0100

    2

--- raichu/lib.py
+++ raichu/lib.py
@@ -1,3 +1,7 @@
-def ag():
-    import antigravity
-    print('voila')
+from sklearn.preprocessing import StandardScaler,RobustScaler
+import pandas as pd
+
+def standardize(X):
+    scaler = StandardScaler()
+    scaler.fit(X)
+    return pd.DataFrame(scaler.transform(X), columns = X.columns)
